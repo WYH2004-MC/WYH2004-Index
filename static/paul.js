3e5093afde11815c39732c0360ae42d812090a49
WYH2004 <32890512+VIPWYH2004@users.noreply.github.com>
2020-03-02 19:06:57 +0800

    1

---
+++ static/paul.js
@@ -0,0 +1,5 @@
+function loader(){var loader=document.getElementById("loader");setTimeout(function(){loader.classList.add("hidden");},2000);}
+loader();function actions(){var sections=document.getElementsByClassName("content")[0].getElementsByTagName("section");var btns=document.getElementsByClassName("actions")[0].getElementsByClassName("item");function act(num){for(var i=0;i<4;i++){btns[i].classList.remove("active");sections[i].classList.remove("active");}
+btns[num].classList.add("active");sections[num].classList.add("active");}
+btns[0].addEventListener("click",function(){act(0)});btns[1].addEventListener("click",function(){act(1)});btns[2].addEventListener("click",function(){act(2)});btns[3].addEventListener("click",function(){act(3)});}
+actions();if(window.console&&window.console.log){console.log("\n %c Sweet %c https://paugram.com \n\n","color: #fff; background: #1979ca; padding:5px 0;","background: #efefef; padding:5px 0;");}
